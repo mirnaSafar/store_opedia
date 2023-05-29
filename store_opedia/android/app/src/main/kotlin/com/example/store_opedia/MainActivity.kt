e66b7f4ab6b11b67f1766cd0d489091405b4d021
mirnaSafar <mirna.safar2000@gmail.com>
2023-05-30 02:42:14 +0300

    commit

--- store_opedia/android/app/src/main/kotlin/com/example/store_opedia/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.store_opedia
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- store_opedia/lib/constant/enums.dart
+++
@@ -1,1 +0,0 @@
-enum AuthMode { login, signup }
